9b10e9087ff06f2bdc7ef50e9d6ebfc3f8547856
Hyun Jin Park <>
2023-10-09 16:12:52 +0900

    :construction: 검색 만든것

---
+++ src/App2.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log("call the api....");
+  }, []);
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("SEARCH FOR", keyword);
+    }    
+  }, [keyword]);
+  
+  return (
+    <div>
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+    </div>
+  );
+}
+
+export default App;
